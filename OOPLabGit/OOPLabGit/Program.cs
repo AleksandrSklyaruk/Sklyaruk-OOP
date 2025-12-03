8ec4a17743d97209d4cdf6a1b54b7efd8e83d03c
ads58 <ads58@W10792>
2025-12-03 20:11:13 +0700

    feature(Program): добавлен ввод двух переменных

--- OOPLabGit/OOPLabGit/Program.cs
+++ OOPLabGit/OOPLabGit/Program.cs
@@ -11,6 +11,8 @@ namespace OOPLabGit
         static void Main(string[] args)
         {
             Console.WriteLine("Я создал второй коммит!");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
